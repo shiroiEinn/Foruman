ed3d1f33709bfc235c6f8fde14568e98b8220cde
Alvin <springbreezever@gmail.com>
2018-12-25 23:11:52 +0700

    Home view with functioning search, minor view edit(sorry)

---
+++ app/Http/Controllers/ForumController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Forum;
+use Illuminate\Http\Request;
+
+class ForumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $forums = Forum::paginate(5);
+        return view('pages.home', compact('forums'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Forum  $forum
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Forum $forum)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Forum  $forum
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Forum $forum)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Forum  $forum
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Forum $forum)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Forum  $forum
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Forum $forum)
+    {
+        //
+    }
+
+    public function search(Request $request){
+        $search = $request->get('name');
+        $forums = Forum::where('postname', 'LIKE', '%' .$search. '%')->paginate();
+        $forums->appends($request->only('name'));
+
+        return view('pages.home', compact('forums'));
+    }
+
+    // public function search(Request $request){
+    //     $search = $request->get('name');
+    //     $pets = Pet::where('name', 'LIKE', '%'.$search.'%')->paginate();
+    //     $pets->appends($request->only('name'));
+
+    //     return view('welcome', compact('pets'));
+
+    // }
+    //small guide from pert 2
+}
